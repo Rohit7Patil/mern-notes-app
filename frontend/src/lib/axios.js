da6f62d1bca516c85d50d7301585dc21e928a69d
Rohit Patil <rohitpatil0522@gmail.com>
2026-01-20 23:40:12 +0530

    axios update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "production" ? "http://localhost:5001/api" : "/api";
+// const BASE_URL = import.meta.env.MODE === "production" ? "http://localhost:5001/api" : "/api";
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api",
 });
 
 export default axiosInstance;
